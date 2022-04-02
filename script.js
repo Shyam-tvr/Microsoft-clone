af0ec53a7e110add009b874cb3346d47fadf3145
Shyam M <64887333+shyam45@users.noreply.github.com>
2022-04-02 18:05:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function validation(event) {
     var username=document.getElementById("username").value;
     var password=document.getElementById("password").value;
     if (username=='user'&&password=='password') {
-        window.location.replace("https://shyam45.github.io/Microsoft-clone/index.html");
+        location.replace("https://shyam45.github.io/Microsoft-clone/index.html");
     }else{
         alert("Login failed ...")
     }
