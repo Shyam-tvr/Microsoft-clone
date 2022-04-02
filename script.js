93c5c6daed521c847100be3bd0a9fed0c78ae308
Shyam M <64887333+shyam45@users.noreply.github.com>
2022-04-02 17:56:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function validation(event) {
+    event.preventDefault();
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+    if (username=='user'&&password=='password') {
+        window.location.replace("/Microsoft-clone/index.html");
+    }else{
+        alert("Login failed ...")
+    }
+}
+
+    if (window.location.href == 'https://shyam45.github.io/Microsoft-clone/index.html') {
+        var a=document.getElementById("user");
+        a.innerHTML= 'user'
+    }
+
+
+
+
+
