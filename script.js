fbbeafa4ae2afe63a93a7a55c48a7013e1d1db8d
Shyam M <64887333+shyam45@users.noreply.github.com>
2022-04-02 17:36:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function validation(event) {
     }
 }
 
-    if (window.location.pathname == '/Microsoft-clone/index.html') {
+    if (window.location.pathname == 'Microsoft-clone/index.html') {
         var a=document.getElementById("user");
         a.innerHTML= 'user'
     }
